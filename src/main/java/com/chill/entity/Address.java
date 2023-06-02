4850b65966aff9a5b856b7cddca3a95174a8c8d1
Gabriel Laza <gabriellaza02@gmail.com>
2023-06-02 05:20:08 +0300

    feat: add field to an address

--- src/main/java/com/chill/entity/Address.java
+++ src/main/java/com/chill/entity/Address.java
@@ -2,8 +2,17 @@ package com.chill.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Address {
+    private Country country;
+
+    private State state;
+
+    private City city;
+
+    private PostalCode postalCode;
 }
